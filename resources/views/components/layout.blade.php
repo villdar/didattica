061d2fa472c1fd2e82794648641a787efeaaece5
Matthias Conte <matth.conte@gmail.com>
2021-11-14 13:16:16 +0100

    Dialog in body - Bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,6 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-{{-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> --}}
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
@@ -153,7 +152,8 @@
     });
 
     $('.summernote').summernote({
-        height: 200
+        height: 200,
+        dialogsInBody: true
     });
 </script>
 
