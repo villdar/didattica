8cd9c99fe4373097737fab665f062159d4c1c2b6
Matthias Conte <matth.conte@gmail.com>
2021-12-03 14:00:34 +0100

    line-clamp removed

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,4 +1,3 @@
-
 @foreach ($posts as $post)
     <div class="mt-16">
         <!-- Versione 1 -->
@@ -39,15 +38,27 @@
         <article class="py-6 mb-4 space-y-6 transition-colors duration-300 border border-black border-opacity-0 bg-gray-50 hover:bg-gray-100 hover:border-opacity-5 rounded-xl">
             <div class="flex flex-col justify-between w-full mx-2 md:mx-4">
                 <div class="justify-between md:flex">
-                <h4 class="mt-2 text-xl font-semibold md:mt-0">
-                    <a href="/posts/{{ $post->slug }}" class="hover:underline">{{ $post->title }}</a>
-                </h4>
+                    <h4 class="mt-2 text-xl font-semibold md:mt-0">
+                        <a href="/posts/{{ $post->slug }}" class="hover:underline">{{ $post->title }}</a>
+                    </h4>
                     <div class="mt-2 md:mr-5">
                         <x-category-button :category="$post->category" />
                     </div>
                 </div>
-                <div class="p-1 mt-3 text-gray-600 line-clamp-3">
-                    {{ $post->excerpt }}
+                <div x-data="{ excerpt: false}" class="m-2">
+                    <div
+                         x-show="!excerpt"
+                         x-cloak
+                         class="p-1 mt-3 mr-1 text-gray-600 cursor-pointer line-clamp-3" @click="excerpt = !excerpt">
+                        {{ $post->excerpt }}
+                    </div>
+                    <div
+                         x-if="!excerpt"
+                         x-cloak
+                         class="p-1 mt-3 text-gray-600"
+                        >
+                        {{ $post->excerpt }}
+                    </div>
                 </div>
                 <div class="flex flex-col justify-between mt-6 md:flex-row md:items-center">
                     <div class="flex items-center space-x-0.5 text-xs font-semibold text-gray-400 md:space-x-1">
