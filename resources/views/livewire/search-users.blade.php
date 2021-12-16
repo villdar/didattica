ac191a6b8d572aa7ec1b5b990b7d6f3f2d2a0e8d
Matthias Conte <matth.conte@gmail.com>
2021-12-16 09:30:27 +0100

    search user

--- resources/views/livewire/search-users.blade.php
+++ resources/views/livewire/search-users.blade.php
@@ -2,7 +2,7 @@
     <input class="p-2" wire:model="search" type="search" placeholder="Cerca l'utente">
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="-my-2 sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                 <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
