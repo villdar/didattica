1a2965aa6a31c45645533b499bf8151e57303dc8
Matthias Conte <matth.conte@gmail.com>
2021-12-16 09:47:17 +0100

    tag-index

--- resources/views/admin/posts/tags/index.blade.php
+++ resources/views/admin/posts/tags/index.blade.php
@@ -1,70 +1,69 @@
 <x-layout>
     <x-setting heading="Tag">
+        <nav class="grid justify-center text-xs text-gray-400 md:flex">
+            <ul class="flex pb-3 space-x-10 font-semibold uppercase border-b-4">
+                <li class="hover:text-blue-400 {{ request()->is('admin/posts/tags/create') ? 'text-blue-500' : '' }}">
+                    <a href="{{ route('tags.create') }}" class="hover:text-blue-500">Crea</a>
+                </li>
+            </ul>
+        </nav>
         <a href="{{ route('tags.create') }}" class="hover:text-blue-500">Crea</a>
-        <div class="py-4">
-            <div class="mx-auto sm:px-6">
-                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
-                    <table class="w-full divide-y divide-gray-200">
-                        <thead class="font-bold bg-gray-50">
-                            <tr>
-                                <th class="px-2 py-3 text-xs tracking-wider text-left uppercase">
-                                    Nome
-                                </th>
-
-                                <th class="px-2 py-3 text-xs tracking-wider text-left uppercase">
-                                    Categoria
-                                </th>
-
-                                <th class="px-2 py-3 text-xs tracking-wider text-left uppercase">
-                                    Slug
-                                </th>
-                                <th class="px-2 py-3 text-xs tracking-wider text-left uppercase">
-                                    Azioni
-                                </th>
-                            </tr>
-                        </thead>
-
-                        <tbody class="text-xs bg-white divide-y divide-gray-200">
-                            @foreach ($tags as $tag)
-
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
                                 <tr>
-                                    <td class="px-2 py-4 whitespace-nowrap">
-                                        {{ $tag->name }}
-                                    </td>
-
-                                    <td class="px-2 py-4 whitespace-nowrap">
-                                        {{ $tag->category_id }}
-                                    </td>
-
-                                    <td class="px-2 py-4 whitespace-nowrap">
-                                        {{ $tag->slug }}
-                                    </td>
-
-                                    <td class="px-2 py-4 text-sm text-gray-500 whitespace-nowrap">
-
-                                        <div class="flex justify-start space-x-1">
-
-                                            <a href="{{ route('tags.edit', $tag) }}" class="p-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-gray-500">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                </svg>
-                                            </a>
-
-                                            <form action="{{ route('tags.delete', $tag) }}" method="POST">
-                                                @csrf
-                                                @method('Delete')
-                                                <button type="submit" class="p-1">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-red-500">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </td>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Nome
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Categoria
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Slug
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                        Azioni
+                                    </th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($tags as $tag)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ $tag->name }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ $tag->category_id }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ $tag->slug }}
+                                        </td>
+                                        <td class="px-2 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                            <div class="flex justify-start space-x-1">
+                                                <a href="{{ route('tags.edit', $tag) }}" class="p-1">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-blue-500">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                    </svg>
+                                                </a>
+                                                <form action="{{ route('tags.delete', $tag) }}" method="POST">
+                                                    @csrf
+                                                    @method('Delete')
+                                                    <button type="submit" class="p-1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 text-red-500">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
