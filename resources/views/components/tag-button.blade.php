d8f076dc9f53c212de3b241fc0c99c5bb1c003b8
Matthias Conte <matth.conte@gmail.com>
2021-12-16 11:55:12 +0100

    tg

--- resources/views/components/tag-button.blade.php
+++ resources/views/components/tag-button.blade.php
@@ -1,12 +1,12 @@
 @props(['tags'])
 <div class="space-y-1" x-data="{ isOpen: false }">
     @foreach ($tags as $tag)
-        @if ($loop->index < 2)
+        @if ($loop->index < 3)
             <a href="/?tags={{ $tag->slug }}"
                class="px-2 py-1 text-xs font-semibold uppercase border-2 border-solid rounded-full"
                style="font-size: 8px; border-color: {{ $tag->category->style }}; color: {{ $tag->category->style }};">{{ $tag->name }}</a>
         @endif
-        @if ($loop->index >= 2)
+        @if ($loop->index >= 3)
 
             <a
                x-cloak
@@ -19,11 +19,11 @@
 
         @endif
     @endforeach
-    <button
-            class="px-3 py-2 transition duration-150 ease-in bg-gray-100 border rounded-full hover:bg-gray-200 h-7"
-            @click="isOpen = !isOpen">
-        <svg fill="currentColor" width="24" height="6">
-            <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)">
-        </svg>
-    </button>
+        <button
+                class="px-3 py-2 transition duration-150 ease-in bg-gray-100 border rounded-full hover:bg-gray-200 h-7"
+                @click="isOpen = !isOpen">
+            <svg fill="currentColor" width="24" height="6">
+                <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)">
+            </svg>
+        </button>
 </div>
