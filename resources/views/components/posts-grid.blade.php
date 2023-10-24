5f6f9754abaf9472559b8e725ae85f7eea4687cf
Matthias Conte <61460340+villdar@users.noreply.github.com>
2023-10-24 12:19:31 +0200

    Update posts-grid.blade.php

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -1,5 +1,5 @@
  <div class="flex p-3 md:pt-2 md:mt-4 w-27">
-     <h2 class="transform translate-y-2">Cerca nella mappa l'attività didattica di tuo interesse e confrontati con gli strumenti proposti</h2>
+     <h2 class="transform translate-y-2">Scopri gli strumenti digitali più adatti alle attività didattiche di tuo interesse. Registrati per tenere traccia dei tuoi strumenti preferiti e condividere commenti. Questo ti aiuterà a scoprire nuovi modi di arricchire il tuo insegnamento attraverso gli strumenti digitali.</h2>
  </div>
  <div class="space-x-6 lg:grid lg:grid-cols-2">
      <article class="bg-chord-bg">
