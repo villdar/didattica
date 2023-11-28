ded070101591e7f2ef9ec13a621d5d5d49c7972a
Matthias Conte <61460340+villdar@users.noreply.github.com>
2023-11-28 13:36:12 +0100

    Update posts-grid.blade.php

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -1,4 +1,4 @@
-     <h2 class="transform translate-y-2 m-2">Ogni giorno uno strumento nuovo col <a href="https://ladigitale.dev/digiquiz/q/6560add1740ff/" class="text-blue-600/75 underline font-bold">Calendario dell'avvento di Didattic@</a>.</h2>
+     <h2 class="transform translate-y-2 m-2">Ogni giorno uno strumento nuovo col <a href="https://ladigitale.dev/digiquiz/q/6560add1740ff/" class="text-blue-500 underline font-bold">Calendario dell'avvento di Didattic@</a>.</h2>
  <div class="flex p-3 md:pt-2 md:mt-4 w-27">
      <h2 class="transform translate-y-2">Scopri gli strumenti digitali più adatti alle attività didattiche di tuo interesse. Registrati per tenere traccia dei tuoi strumenti preferiti e condividere commenti. Questo ti aiuterà a scoprire nuovi modi di arricchire il tuo insegnamento attraverso gli strumenti digitali.</h2>
  </div>
